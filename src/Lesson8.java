dc52e09967343faec9e17470cdfe3d7f944e96bc
Adrian <adrian.korzan@gmail.com>
2021-03-25 00:14:45 +0100

    Lesson 8

--- src/Lesson8.java
+++ src/Lesson8.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Lesson8 {
     public static void main(String[] args)
@@ -8,8 +9,20 @@ public class Lesson8 {
         int answer;
 
         Random rand = new Random();
-        for (int i = 100; i > 0; i--) {
-//            System.out.println(rand.);
-        }
+
+        random = rand.nextInt(10) + 1;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Zgadnij liczbę od 1 do 10!");
+
+        do {
+            triesNumber++;
+            System.out.println("Podaj liczbę:");
+            answer = scanner.nextInt();
+
+            if (answer > random) System.out.println("Nie zgadłeś, moja liczba jest mniejsza");
+            else if (answer < random) System.out.println("Nie zgadłeś, moja liczba jest większa");
+        } while (answer != random);
+        System.out.println("Brawo, odgadłeś za " + triesNumber + " razem!");
     }
 }
