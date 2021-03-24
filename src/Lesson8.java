e5df649f8e868471d642f928011c7509ed346df6
Korzanicho <adrianyamakasi@wp.pl>
2021-03-24 22:26:31 +0100

    none

---
+++ src/Lesson8.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class Lesson8 {
+    public static void main(String[] args)
+    {
+        int triesNumber = 0;
+        int random;
+        int answer;
+
+        Random rand = new Random();
+        for (int i = 100; i > 0; i--) {
+//            System.out.println(rand.);
+        }
+    }
+}
