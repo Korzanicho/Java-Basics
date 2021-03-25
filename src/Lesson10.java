3b6779b4dfd5bff044d33300b6299fb910e0700f
Adrian <adrian.korzan@gmail.com>
2021-03-25 18:13:39 +0100

    Lesson 10 - forEach

---
+++ src/Lesson10.java
@@ -0,0 +1,10 @@
+public class Lesson10 {
+    public static void main(String[] args)
+    {
+        String names[]= {"Adrian", "Klauda", "Artur", "Kamil"};
+
+        for(String name : names){
+            System.out.println(name);
+        }
+    }
+}
