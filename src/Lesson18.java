ba4730f44305ae25c6d7a1147cc9d52a254f3a9c
Adrian <adrian.korzan@gmail.com>
2021-03-29 22:55:17 +0200

    Lesson18 - Math

---
+++ src/Lesson18.java
@@ -0,0 +1,17 @@
+public class Lesson18 {
+    public static void main(String[] args)
+    {
+        int a = 5;
+        int b = 11;
+        System.out.println(Math.max(a, b));
+        System.out.println(Math.min(a, b));
+        System.out.println(Math.abs(a));
+        System.out.println(Math.pow(a, 2));
+        System.out.println(Math.sqrt(9));
+        System.out.println(Math.round(5.5)); // zaokrąglanie
+        System.out.println(Math.ceil(5.3)); // zaokrąglanie w górę
+        System.out.println(Math.floor(5.7)); // zaokrąglanie w dół
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+    }
+}
